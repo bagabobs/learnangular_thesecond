33e2da94bc8eb7e37e173b708ebd2466d7875bb9
baga gusdiana <baga.gusdiana@len.co.id>
2020-08-03 14:42:33 +0700

    auth service

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,109 @@
+import {BehaviorSubject, Observable, throwError} from 'rxjs';
+import {IUser, User} from '../user/user/user';
+import {Role} from './auth.enum';
+import { Injectable } from '@angular/core';
+import {catchError, filter, map, tap} from 'rxjs/operators';
+import {flatMap} from 'tslint/lib/utils';
+import {transformError} from '../common/common';
+import * as decode from 'jwt-decode';
+import {CacheService} from './cache.service';
+
+export interface IAuthStatus {
+  isAuthenticated: boolean;
+  userRole: Role;
+  userId: string;
+}
+
+export interface IServerAuthResponse {
+  accessToken: string;
+}
+
+export const defaultAuthStatus: IAuthStatus = {
+  isAuthenticated: false,
+  userRole: Role.None,
+  userId: '',
+};
+
+export interface IAuthService {
+  readonly authStatus$: BehaviorSubject<IAuthStatus>;
+  readonly currentUser$: BehaviorSubject<IUser>;
+  login(email: string, password: string): Observable<void>;
+  logout(clearToken?: boolean): void;
+  getToken(): string;
+}
+@Injectable()
+export abstract class AuthService extends CacheService implements IAuthService {
+  readonly authStatus$ = new BehaviorSubject<IAuthStatus>(defaultAuthStatus);
+  readonly currentUser$ = new BehaviorSubject<IUser>(new User());
+  protected abstract authProvider(
+    email: string,
+    password: string
+  ): Observable<IServerAuthResponse>;
+  protected abstract transformJwtToken(token: unknown): IAuthStatus;
+  protected abstract getCurrentUser(): Observable<User>;
+  constructor() {
+    super();
+    if (this.hasExpiredToken()) {
+      this.logout(true);
+    } else {
+      this.authStatus$.next(this.getAuthStatusFromToken());
+    }
+  }
+
+
+  getToken(): string {
+    return this.getItem('jwt') ?? '';
+  }
+
+  protected setToken(jwt: string): void {
+    this.setItem('jwt', jwt);
+  }
+
+  protected clearToken(): void {
+    this.removeItem('jwt');
+  }
+
+  login(email: string, password: string): Observable<void> {
+    this.clear();
+    const loginResponse$ = this.authProvider(email, password)
+      .pipe(
+        map((value) => {
+          this.setToken(value.accessToken);
+          const token = decode(value.accessToken);
+          return this.transformJwtToken(token);
+        }),
+        tap((status) => this.authStatus$.next(status)),
+        filter((status: IAuthStatus) => status.isAuthenticated),
+        flatMap(() => this.getCurrentUser()),
+        map(user => this.currentUser$.next(user)),
+        catchError(transformError)
+      );
+    loginResponse$.subscribe({
+      error: err => {
+        this.logout();
+        return throwError(err);
+      }
+    });
+    return loginResponse$;
+  }
+
+  logout(clearToken?: boolean): void {
+    if (clearToken) {
+      this.clearToken();
+    }
+    setTimeout(() => this.authStatus$.next(defaultAuthStatus), 0);
+  }
+
+  protected hasExpiredToken(): boolean {
+    const jwt = this.getToken();
+    if (jwt) {
+      const payload = decode(jwt) as any;
+      return Date.now() >= payload.exp * 1000;
+    }
+    return true;
+  }
+
+  protected getAuthStatusFromToken(): IAuthStatus {
+    return this.transformJwtToken(decode(this.getToken()));
+  }
+}
---
+++ src/app/auth/cache.service.ts
@@ -0,0 +1,27 @@
+export abstract class CacheService {
+
+  constructor() { }
+
+  protected getItem<T>(key: string): T | null {
+    const data = localStorage.getItem(key);
+    if (data != null) {
+      return JSON.parse(data);
+    }
+    return null;
+  }
+
+  protected setItem(key: string, data: object | string): void {
+    if (typeof data === 'string') {
+      localStorage.setItem(key, data);
+    }
+    localStorage.setItem(key, JSON.stringify(data));
+  }
+
+  protected removeItem(key: string): void {
+    localStorage.removeItem(key);
+  }
+
+  protected clear(): void {
+    localStorage.clear();
+  }
+}
